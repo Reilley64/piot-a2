190c5679d3631a26e92ceeef0b4d58ab8a3ee842
sam radford <sam.radford@capitalroad.com.au>
2019-05-20 16:51:40 +1000

    search added and sql added

--- menu.py
+++ menu.py
@@ -5,9 +5,18 @@ def print_menu():
     print "2. Borrow"
     print "3. Return"
     print "4. Logout"
-    print 67 * "-"    
+    print 67 * "-"
 
-def check_if_user_exists(user):
+def print_search():
+    print 30 * "-" , "Search By" , 30 * "-"
+    print "1. Title"
+    print "2. Author"
+    print "3. Date"
+    print "4. Back"
+    print 67 * "-"
+        
+
+def check_if_user_doesnt_exist(user):
     sql = "select * from lmsUser where username = '{user}'"
     result = dbconnection.cloudConnection('GET', sql)
     if(result.length == 1):
@@ -18,28 +27,35 @@ def check_if_user_exists(user):
 def create_user(user, name):
     sql = "insert into lmsUser (username, name) values ('{user}', '{name}')"
     result = dbconnection.cloudConnection('POST', sql)
+    return result
 
 
-def borrow_book():
+def borrow_book(user, name, bookTitle):
     ## get user
-
+    if (check_if_user_doesnt_exist(user)):
+        create_user(user, name)
+    sql = "select id from lmsUser where username = '{user}'"
+    userID = dbconnection.cloudConnection('GET', sql)
     ## get book
+    sql2 = "select id from book where title = {bookTitle}"
+    bookID = dbconnection.cloudConnection('GET', sql2)
 
     ## update book table
-    sql = "insert into bookBorrowed values (1, 1, 1, 'BORROWED', NOW(), null)"
+    sql3 = "insert into bookBorrowed values (1, 1, {bookID}, 'BORROWED', NOW(), null)"
+    result = dbconnection.cloudConnection('POST', sql3)
 
-
-def return_book():
-    ## get user
-
+def return_book(bookTitle):
     ## get book
-
+    sql1 = "select id from book where title = {bookTitle}"
+    bookID = dbconnection.cloudConnection('GET', sql1)
     ## update book
+    sql2 = "update bookBorrowed set status = 'RETURNED', returnDate = NOW() where bookID = {bookID} "
+    result = dbconnection.cloudConnection('POST', sql2)
 
-def search_book():
-    ## full text serach or like search based on fields.
-    ## check assignment
-
+def search_book(column, query):
+    sql = "SELECT * FROM book WHERE {column} LIKE '%{query}%'"
+    result = dbconnection.cloudConnection('GET', sql)
+    return result
 
 def logout():
     ## send socket back to RP
@@ -52,6 +68,26 @@ while true:
      
     if choice==1:     
         print "search has been selected"
+        while true:
+            print_search()
+            search = input("Enter your choice [1-4]: ")
+            if search == 1:
+                print "search by title selected"
+                query = input("Enter your search for title")
+                search_book('title', query)
+            elif search == 2:
+                print "search by author selected"
+                query = input("Enter your search for author")
+                search_book('author', query)
+            elif search == 3:
+                print "search by date selected"
+                query = input("Enter your search for date")
+                search_book('date', query)
+            elif search == 4:
+                break
+                print "back selected"
+            else:
+                raw_input("Wrong option selection. Enter any key to try again..")
     elif choice==2:
         print "borrow book has been selected"
     elif choice==3:
