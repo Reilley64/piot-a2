f139dbb8c364694158195c70eec6e07b2fe36552
Reilley Gray <reilleygray@gmail.com>
2019-05-21 23:46:52 +1000

    Adding books with admin website

---
+++ host/flask/classlib/database.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+
+class Database():
+    def __init__(self):
+        self.connection = mysql.connector.connect(
+            host="35.201.1.71",
+            database="librarylms",
+            user="root",
+        )
+
+    def getAllBooks(self):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM book;")
+        return cursor.fetchall()
+
+    def addBook(self, title, author, publishedDate):
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO book (title, author, publishedDate) VALUES (%s, %s, %s)",
+                       (title, author, publishedDate))
+        self.connection.commit
--- host/flask/webserver.py
+++ host/flask/webserver.py
@@ -1,21 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
+from classlib.database import Database
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+database = Database()
 username = None
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    global database
     global username
 
-    if not username:
-        return redirect(url_for("login"))
+    if request.method == "POST":
+        database.addBook(request.form["title"], request.form["author"], request.form["publishedDate"])
+
+        books = database.getAllBooks()
+        return render_template("index.html", books=books)
     else:
-        return render_template("index.html")
+        if not username:
+            return redirect(url_for("login"))
+        else:
+            books = database.getAllBooks()
+            return render_template("index.html", books=books)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -24,9 +34,17 @@ def login():
 
     if request.method == "POST":
         if request.form["username"] == "jaqen" and request.form["password"] == "hghar":
-            username = "jaqen"
+            username = request.form["username"]
             return redirect(url_for("index"))
         else:
-            return redirect(url_for("login"))
+            return render_template("login.html")
     else:
         return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    global username
+
+    username = None
+    return redirect(url_for("index"))
