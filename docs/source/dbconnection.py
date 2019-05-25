38883a98e37d16420c5b2153301fa0ec6d79e2fe
sam radford <sam.radford@capitalroad.com.au>
2019-05-25 16:30:10 +1000

    started documentation with autodoc

---
+++ docs/source/dbconnection.py
@@ -0,0 +1,38 @@
+import pymysql.cursors
+import pymysql
+
+class dbconnection:
+    def __init__(self):
+        self.pymysql = pymysql
+
+    def connect(self):
+        return self.pymysql.connect(user='root',
+            password='',
+            host='35.201.1.71',
+            db='librarylms',
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor)
+
+    def cloudConnection(self, method, sql):
+        connection = self.connect()
+        if('GET' == method):
+            try:
+                with connection.cursor() as cursor:
+                    cursor.execute(sql)
+                    result = cursor.fetchall()
+                    return result
+            finally:
+                connection.close()
+        elif('POST' == method):
+            try:
+                with connection.cursor() as cursor:
+                    cursor.execute(sql)
+                    connection.commit()
+            finally:
+                connection.close()
+        else:
+            connection.close()
+            return 'false'
+
+
+    
