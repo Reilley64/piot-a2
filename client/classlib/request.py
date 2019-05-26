0dece2cf61edb4f05f7c2f30b123c52e227aa95e
Reilley Gray <reilleygray@gmail.com>
2019-05-26 23:35:59 +1000

    Voice search feature

--- client/classlib/request.py
+++ client/classlib/request.py
@@ -2,7 +2,7 @@ import json
 
 
 class Request:
-    #Requests for all main features 
+    """Request class to handle interactions with host pi"""
     request = None
     bookID = None
     column = None
@@ -11,24 +11,29 @@ class Request:
     username = None
 
     def bookBorrow(self, bookID):
+        """Defines the request as a book borrow"""
         self.request = "borrow"
         self.bookID = bookID
 
     def bookReturn(self, bookID):
+        """Defines the request as a book return"""
         self.request = "return"
         self.bookID = bookID
 
     def bookSearch(self, column, query):
+        """Defines the request as a book search"""
         self.request = "search"
         self.column = column
         self.query = query
 
     def credentials(self, username, name):
+        """Defines the request as credentials"""
         self.request = "credentials"
         self.username = username
         self.name = name
 
     def __str__(self):
+        """Handles turning the class into a JSON String for sending to host pi"""
         if self.request is None:
             return False;
 
